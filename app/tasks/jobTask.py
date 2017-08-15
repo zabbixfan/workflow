1340cb5f68e988318f9394f33f0d210ca35fe5a6
manatee <songcheng3215@tops001.com>
2017-08-15 12:41:20 +0800

    增加agent同步机房虚拟机功能

--- app/tasks/jobTask.py
+++ app/tasks/jobTask.py
@@ -11,7 +11,7 @@ from .mailTask import applyMail
 from app.models.tickets import Tickets
 @celery.task()
 def createProjectJob(query):
-    print json.dump(query,indent=4)
+    print json.dumps(query,indent=4)
     data = query['data']
     repoUrl = ''
     #search repo group

11b769dbefe310792c874482c427f309f5a2449b
manatee <songcheng3215@tops001.com>
2017-08-31 10:31:31 +0800

    增加关闭zerorpc连接

--- app/tasks/saveProject.py
+++ app/tasks/saveProject.py
@@ -12,6 +12,7 @@ def saveProjcetToConsul(pid):
     c=consul.Consul()
     BasekitClient = Client(Config.ONEKIT_RPC, timeout=3000, passive_heartbeat=False)
     res = BasekitClient.GetProjectInfo(pid)
+    BasekitClient.close()
     port = 8000+int(pid)
     projectType = getProjectType(pid)
     projectName = res['name']
